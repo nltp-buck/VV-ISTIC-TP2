94ef054a30ef6f334677e281763c742b4b3a7423
NLTP_ASHES <a.s.h.e.s.343@gmail.com>
2023-12-11 09:33:18 +0100

    Exercice 4 (WIP)

--- code/javaparser-starter/src/main/java/fr/istic/vv/NoGetterPrinter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/NoGetterPrinter.java
@@ -2,19 +2,19 @@ package fr.istic.vv;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.nodeTypes.modifiers.NodeWithAccessModifiers;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 // This class visits a compilation unit and
 // prints all public enum, classes or interfaces along with their public methods
 public class NoGetterPrinter extends VoidVisitorWithDefaults<Void> {
 
-    private List<String> privateAttributes = new ArrayList<>();
-    private List<String> publicMethods = new ArrayList<>();
-    private List<String> faultyArttributes = new ArrayList<>();
+    private final List<String> privateAttributes = new ArrayList<>();
+    private final List<String> publicMethods = new ArrayList<>();
+    private final List<FaultyAttribute> faultyArttributes = new ArrayList<>();
 
     @Override
     public void visit(CompilationUnit unit, Void arg) {
@@ -25,7 +25,8 @@ public class NoGetterPrinter extends VoidVisitorWithDefaults<Void> {
 
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
 
-        System.out.println("[i] TypeDecl : " + declaration.getName());
+        String clazz = declaration.getNameAsString();
+        System.out.println("[i] Class : " + clazz);
 
         for (FieldDeclaration field : declaration.getFields()) {
             field.accept(this, arg);
@@ -38,13 +39,13 @@ public class NoGetterPrinter extends VoidVisitorWithDefaults<Void> {
         for (String attribute : privateAttributes) {
             List<String> publicGetters = publicMethods.stream().filter((String method) -> method.toLowerCase().matches("get" + attribute.toLowerCase())).toList();
             if (publicGetters.isEmpty()) {
-                faultyArttributes.add(attribute);
+                faultyArttributes.add(new FaultyAttribute(attribute, clazz));
             }
         }
 
         System.out.println("[i] REPORT :");
-        for (String attribute : faultyArttributes) {
-            System.out.println("[i] No Public Getter : " + attribute);
+        for (FaultyAttribute attribute : faultyArttributes) {
+            System.out.println("[i] '" + attribute.attribute + "' attribute from class '" + attribute.clazz + "' is private and has no public getter.");
         }
     }
 
@@ -61,7 +62,7 @@ public class NoGetterPrinter extends VoidVisitorWithDefaults<Void> {
     @Override
     public void visit(FieldDeclaration declaration, Void arg) {
         for (VariableDeclarator var : declaration.getVariables()) {
-            System.out.println("[i] FieldDecl : (" + declaration.isPublic() + ") " + var.getNameAsString());
+            System.out.println("[i]     Attribute : (" + GetVisibilityAsString(declaration) + ") " + var.getNameAsString());
             if (declaration.isPrivate()) {
                 privateAttributes.add(var.getNameAsString());
             }
@@ -70,9 +71,29 @@ public class NoGetterPrinter extends VoidVisitorWithDefaults<Void> {
 
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
-        System.out.println("[i] MethodDecl : " + declaration.getName());
+        System.out.println("[i]     Method : (" + GetVisibilityAsString(declaration) + ") " + declaration.getName());
         if (declaration.isPublic()) {
             publicMethods.add(declaration.getNameAsString());
+        }
+    }
+
+    private String GetVisibilityAsString(NodeWithAccessModifiers<?> declaration) {
+        if (declaration.isPublic()) {
+            return "Public";
+        }
+        if (declaration.isPrivate()) {
+            return "Private";
+        }
+        return "Unknown";
+    }
+
+    static class FaultyAttribute {
+        String attribute;
+        String clazz;
+
+        FaultyAttribute(String attribute, String clazz) {
+            this.attribute = attribute;
+            this.clazz = clazz;
         }
     }
 }
